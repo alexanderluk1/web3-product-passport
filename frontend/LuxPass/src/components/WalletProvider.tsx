87a87d872185eade1c77d7c79c96bc1337e51ae9
DamienChew2001 <damienchew2001@gmail.com>
2026-04-14 00:51:43 +0800

    removed the custom network config at WalletProvider in frontend

--- frontend/LuxPass/src/components/WalletProvider.tsx
+++ frontend/LuxPass/src/components/WalletProvider.tsx
@@ -8,13 +8,6 @@ interface WalletProviderProps {
   children: ReactNode;
 }
 
-const localAptosConfig = new AptosConfig({
-  network: Network.CUSTOM,
-  fullnode: "http://127.0.0.1:8080/v1",
-  indexer:  "http://127.0.0.1:8090/v1/graphql",
-  faucet:   "http://127.0.0.1:8082",
-});
-
 export const WalletProvider = ({ children }: WalletProviderProps) => {
   const handleError = (error: Error) => {
     console.error("🔗 Wallet Provider Error:", error);
@@ -22,7 +15,6 @@ export const WalletProvider = ({ children }: WalletProviderProps) => {
 
   return (
     <AptosWalletAdapterProvider
-      aptosConfig={localAptosConfig}
       autoConnect={true}
       onError={handleError}
     >
